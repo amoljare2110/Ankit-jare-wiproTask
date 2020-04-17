8e4c8094409e2bdebbc18166c62d1f05a24b3ed0
Amol <amol@bruviti.com>
2020-04-17 13:25:04 +0530

    Added unit test cases

---
+++ app/src/test/java/DataSuccess.kt
@@ -0,0 +1,13 @@
+import com.ankit.jare.model.ListRepository
+import org.junit.Assert
+import org.junit.Test
+
+class DataSuccess {
+
+    @Test
+    fun getData() {
+        ListRepository.getInstance().getRepoList { isSuccess, response ->
+            Assert.assertTrue("Response Succesfull:", isSuccess)
+        }
+    }
+}
